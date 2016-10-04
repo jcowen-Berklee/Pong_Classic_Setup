9a98b3079354d1601c7b0b37f715bb4e6dede146
jcowen-Berklee <jcowen@berklee.edu>
2016-10-04 14:12:23 -0400

    Basic Setup
    Added in the game objects and scripts.

---
+++ Assets/Scripts/ballDynamic.cs
@@ -0,0 +1,36 @@
+﻿//LMSC-281 Logic and Programming
+//example ball startup script that uses the physics system
+//Fall 2016 Jeanine Cowen
+
+using UnityEngine;
+using System.Collections;
+
+public class ballDynamic : MonoBehaviour {
+
+	//this will hold the rigidbody component of the object, so that it can then be adjusted by the physics engine
+	public Rigidbody rb;
+
+	//this is not truly necessary, just used to give the object a known direction when the script is initialized
+	public GameObject targetObject;
+
+	//a variable to control the force applied to the ball
+	float ballForce = 10.0f;
+
+
+	void Start() {
+
+		//this loads the rigidbody component of the object to which this script is applied
+		rb = GetComponent<Rigidbody>();
+
+		//this rotates the object toward a defined location, drag an object in the inspector onto the targetObject field
+		transform.LookAt (targetObject.transform);
+	}
+		
+	//FixedUpdate is the proper function to place all physics updates
+	void FixedUpdate() {
+
+		//now that the object is faced toward a target apply a force and move it forward
+		rb.AddForce(transform.forward * ballForce);
+	}
+
+}
---
+++ Assets/Scripts/ballKinematic.cs
@@ -0,0 +1,32 @@
+﻿//LMSC-281 Logic and Programming
+//example ball startup script which uses the transform.Translate set of Kinematic functions
+//Fall 2016 Jeanine Cowen
+
+using UnityEngine;
+using System.Collections;
+
+public class ballKinematic : MonoBehaviour {
+
+	//a variable to control the speed of the ball
+	float ballSpeed = 5.0f;
+
+
+	void Start() {
+	
+	}
+
+
+	void Update () {
+
+		//we are using the transform component to move the object, there are many more options that can be found here:
+		//http://docs.unity3d.com/ScriptReference/Transform.Translate.html
+		//http://docs.unity3d.com/ScriptReference/Vector3.html
+
+		//also note the use of a time function/method
+		//http://docs.unity3d.com/ScriptReference/Time-deltaTime.html
+
+//		transform.Translate(Vector2.left * ballSpeed*Time.deltaTime);
+		transform.Translate (new Vector2(-0.5f, -0.5f)* ballSpeed*Time.deltaTime);
+	}
+
+}
